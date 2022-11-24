3aa049945d645324a96be1c702a7f2318ed33f41
Serg Kotovsky <rokulab@mail.ru>
2022-11-25 00:05:08 +0300

    add prefix "Err" to errors names, package auth

--- temp-stor-auth-dev/internal/domain/auth/errors.go
+++ temp-stor-auth-dev/internal/domain/auth/errors.go
@@ -3,7 +3,7 @@ package auth
 import "errors"
 
 var (
-	PassIncorrect = errors.New("password mismatch")
-	EmptySecret   = errors.New("empty secret key for token creation")
-	ZeroDuration  = errors.New("zero duration for token creation")
+	ErrPassIncorrect = errors.New("password mismatch")
+	ErrEmptySecret   = errors.New("empty secret key for token creation")
+	ErrZeroDuration  = errors.New("zero duration for token creation")
 )
