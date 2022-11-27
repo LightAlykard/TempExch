329c6e2d3c0e2b094ce2aefbc7e2e8309e7c7833
Serg Kotovsky <rokulab@mail.ru>
2022-11-28 00:08:45 +0300

    add some errors for storage layer

--- temp-stor-auth-dev/internal/adapters/storage/errors.go
+++ temp-stor-auth-dev/internal/adapters/storage/errors.go
@@ -3,5 +3,9 @@ package storage
 import "errors"
 
 var (
-	NotFound = errors.New("user with specified login was not found")
+	NotFound   = errors.New("user with specified login was not found")
+	UserExists = errors.New("given login is already in use")
+	EmptyLogin = errors.New("login should not be empty")
+	EmpthPass  = errors.New("pass should not be empty")
+	NoSuchUser = errors.New("there is no user with such login")
 )
